43b126025bcf415d8a612f57e6ba5c18cbcd18c9
shilan_hk <shilan_hk@yahoo.com>
2020-05-01 16:48:07 +0430

    Iage picker and manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -4,7 +4,8 @@ import { Card, Icon, Button, Input, CheckBox } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
-//import { ImagePicker } from 'expo';
+import { Asset } from 'expo';
+import * as ImageManipulator from 'expo-image-manipulator';
 import { createBottomTabNavigator } from 'react-navigation';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -151,9 +152,21 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({ imageUrl: capturedImage.uri });
+                this.processImage(capturedImage.uri);
             }
         }
+    }
+
+    processImage = async (imageUri) => {
+        let processedImage = await ImageManipulator.manipulateAsync(
+            imageUri,
+            [
+                { resize: { width: 400 } }
+            ],
+            { format: 'png' }
+        );
+        console.log(processedImage);
+        this.setState({ imageUrl: processedImage.uri });
     }
 
 
